86050c804f56f4b88c28abe5edee5a0f7839fd92
Sathvik PN <sathvikapn@gmail.com>
2021-05-29 12:54:36 +0530

    add application layout

--- text_editor_app.py
+++ text_editor_app.py
@@ -6,9 +6,19 @@ window.title("Simple Text Editor")
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
+# Create required Widgets
 txt_edit = tk.Text(window)
 fr_buttons = tk.Frame(window)
 btn_open = tk.Button(fr_buttons, text="Open")
 btn_save = tk.Button(fr_buttons, text="Save As...")
+
+# application layout
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+
+fr_buttons.grid(row=0, column=0, sticky="ns")
+txt_edit.grid(row=0, column=1, sticky="nsew")
+
+
 
 window.mainloop()
