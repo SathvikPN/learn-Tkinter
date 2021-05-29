984b70e4470d8fe1c51c359db84d9cff0759b6b8
Sathvik PN <sathvikapn@gmail.com>
2021-05-29 12:00:14 +0530

    add functions for updating label value

--- count_logger.py
+++ count_logger.py
@@ -1,4 +1,13 @@
+# functions has to be defined before they are used. so put in top level
+
 import tkinter as tk
+def increase():
+    value = int(lbl_value["text"])
+    lbl_value["text"] = f"{value + 1}"
+
+def decrease():
+    value = int(lbl_value["text"])
+    lbl_value["text"] = f"{value - 1}"
 
 window = tk.Tk()
 
@@ -7,13 +16,23 @@ window.columnconfigure([0, 1, 2], minsize=50, weight=1)
 window.minsize(width=500, height=50)
 window.title("Count-logger")
 
-btn_decrease = tk.Button(master=window, text="-")
+btn_decrease = tk.Button(
+    master=window, 
+    text="-",
+    command=decrease    
+)
 btn_decrease.grid(row=0, column=0, sticky="nsew")
 
 lbl_value = tk.Label(master=window, text="0")
 lbl_value.grid(row=0, column=1)
 
-btn_increase = tk.Button(master=window, text="+")
+btn_increase = tk.Button(
+    master=window, 
+    text="+",
+    command=increase
+)
 btn_increase.grid(row=0, column=2, sticky="nsew")
+
+
 
 window.mainloop()
