9c720a604114e2c5fd049422a240824125f33fa7
Sathvik PN <sathvikapn@gmail.com>
2021-05-29 13:05:58 +0530

    add function: save_file

--- text_editor_app.py
+++ text_editor_app.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import asksaveasfilename
 
 
 def open_file():
@@ -15,6 +16,22 @@ def open_file():
         txt_edit.insert(tk.END, text)
     window.title(f"Simple Text Editor - {filepath}")
 
+
+def save_file():
+    """Save the current file as a new file."""
+    filepath = asksaveasfilename(
+        defaultextension="txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
+    )
+    if not filepath:
+        return
+    with open(filepath, "w") as output_file:
+        text = txt_edit.get("1.0", tk.END)
+        output_file.write(text)
+    window.title(f"Simple Text Editor - {filepath}")
+
+
+
 window = tk.Tk()
 window.title("Simple Text Editor")
 
@@ -25,7 +42,7 @@ window.columnconfigure(1, minsize=800, weight=1)
 txt_edit = tk.Text(window)
 fr_buttons = tk.Frame(window)
 btn_open = tk.Button(fr_buttons, text="Open", command=open_file)
-btn_save = tk.Button(fr_buttons, text="Save As...")
+btn_save = tk.Button(fr_buttons, text="Save As...", command=save_file)
 
 # application layout
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
